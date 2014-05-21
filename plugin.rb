18fbc0746679379e48341c03dd005862da6f3312
Nocte- <nocte.unicorn@gmail.com>
2014-05-21 19:38:19 +0200

    Add header with info

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,9 @@
+# name: Hexahedra Forum Style
+# about: Make the category page look good
+# version: 0.1
+# author: Nocte
+# minimum Discourse version: v0.9.9.3
+
 register_asset "javascripts/colored_borders.js"
 register_asset "javascripts/discourse/templates/discovery/categories.js.handlebars"
 
