a8fc7146e76630b0c50611efb750f35171d3afa5
Nocte- <nocte.unicorn@gmail.com>
2014-06-04 22:26:54 +0200

    Update for 0.9.9.6

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: Hexahedra Forum Style
 # about: Make the category page look good
-# version: 0.1
+# version: 0.2
 # author: Nocte
-# minimum Discourse version: v0.9.9.3
+# minimum Discourse version: v0.9.9.6
 
 register_asset "javascripts/colored_borders.js"
 register_asset "javascripts/discourse/templates/discovery/categories.js.handlebars"
