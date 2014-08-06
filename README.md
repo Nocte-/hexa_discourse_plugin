26272354ecae4083e175f8a536ce5beb8cd17b32
Nocte <nocte.unicorn@gmail.com>
2014-08-06 13:28:05 +0200

    Removed: colored borders are now standard in Discourse

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ Hexahedra Forum Discourse Plugin
 ================================
 
 Just something to hold the modifications for forum.hexahedra.net.
-Based on the style of discuss.newrelic.com
 
--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,6 @@
 # name: Hexahedra Forum Style
-# about: Make the category page look good
-# version: 0.4
+# about: No longer needed
+# version: 0.5
 # author: Nocte
-# minimum Discourse version: v0.9.9.10
-
-register_asset "javascripts/colored_borders.js"
-register_asset "javascripts/discourse/templates/discovery/categories.js.handlebars"
+# minimum Discourse version: v0.9.9.15
 
